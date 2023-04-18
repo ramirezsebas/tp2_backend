11d2228f24e0ecca7e04352f5ce51db86c5ba47b
Matias Sebastian Ramirez Brizuela <ramirezmatias946@gmail.com>
2023-04-18 01:00:25 -0400

    feat: handler de reservas

---
+++ src/pages/api/reservas/[id].ts
@@ -0,0 +1,9 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { PrismaClient } from "@prisma/client";
+import type { NextApiRequest, NextApiResponse } from "next";
+
+const prisma = new PrismaClient();
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+
+}
