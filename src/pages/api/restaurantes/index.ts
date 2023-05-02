c547967218a8d25ca2606c18f1d193c9836efc9e
Matias Sebastian Ramirez Brizuela <ramirezmatias946@gmail.com>
2023-05-02 19:28:45 -0400

    refactor: cambiamos a async await lo que seria el endpoint de restaurantes

--- src/pages/api/restaurantes/index.ts
+++ src/pages/api/restaurantes/index.ts
@@ -1,17 +1,31 @@
 import { PrismaClient } from "@prisma/client";
+import { PrismaClientInitializationError } from "@prisma/client/runtime/library";
 import { NextApiRequest, NextApiResponse } from "next";
 
 const prisma = new PrismaClient();
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
   const method = req.method;
   const { nombre, direccion } = req.body;
 
   switch (method) {
     case "GET":
-      prisma.restaurante.findMany().then((data) => {
-        res.status(200).json(data);
-      });
+      try {
+        let restaurantes = await prisma.restaurante.findMany();
+        res.status(200).json(restaurantes);
+      } catch (error) {
+        if (error instanceof PrismaClientInitializationError) {
+          res.status(500).json({
+            message: mapPrismaClientError(error.errorCode ?? ""),
+          });
+        } else {
+          res.status(500).json(error);
+        }
+      }
+
       break;
     case "POST":
       if (!nombre || !direccion) {
@@ -21,20 +35,51 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
         return;
       }
 
-      prisma.restaurante
-        .create({
+      try {
+        let restauranteCreado = await prisma.restaurante.create({
           data: {
             nombre: nombre,
             direccion: direccion,
           },
-        })
-        .then((data) => {
-          res.status(200).json(data);
         });
+
+        res.status(201).json(restauranteCreado);
+      } catch (error) {
+        if (error instanceof PrismaClientInitializationError) {
+          res.status(500).json({
+            message: mapPrismaClientError(error.errorCode ?? ""),
+          });
+        } else {
+          res.status(500).json(error);
+        }
+      }
 
       break;
     default:
       res.setHeader("Allow", ["GET", "POST"]);
       res.status(405).end(`Metodo ${method} no permitido`);
+  }
+}
+
+function mapPrismaClientError(errorCode: string) {
+  switch (errorCode) {
+    case "P101":
+      return "La URL de la base de datos es inválida o tiene un formato incorrecto.";
+    case "P102":
+      return "No se pudo conectar al servidor de la base de datos o se rechazó la conexión.";
+    case "P103":
+      return "No se pudo autenticar con la base de datos debido a que las credenciales proporcionadas son inválidas o insuficientes.";
+    case "P104":
+      return "No se pudo inicializar el cliente de Prisma porque el archivo 'schema.prisma' no se encontró o tiene un formato incorrecto.";
+    case "P105":
+      return "No se pudo inicializar el cliente de Prisma porque hubo un error al analizar el archivo 'schema.prisma'.";
+    case "P106":
+      return "No se pudo inicializar el cliente de Prisma porque no se pudo generar el esquema de Prisma a partir del archivo 'schema.prisma'.";
+    case "P107":
+      return "No se pudo inicializar el cliente de Prisma debido a que hubo un error durante el proceso de migración de la base de datos.";
+    case "P108":
+      return "No se pudo inicializar el cliente de Prisma debido a que hubo un error durante el proceso de generación del cliente.";
+    default:
+      return "Ocurrió un error desconocido al inicializar el cliente de Prisma.";
   }
 }
